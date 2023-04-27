4303e6017f17cd09a6cf1a2c62eb59c039368e77
Liam <hxl1119@case.edu>
2023-04-27 05:28:22 -0400

    test

--- test.py
+++ test.py
@@ -1,32 +1,14 @@
-import sys
-from PySide2.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog
-from PySide2.QtMultimedia import QMediaPlayer, QMediaContent
-from PySide2.QtMultimediaWidgets import QVideoWidget
+import cv2
 
-class VideoPlayer(QMainWindow):
-    def __init__(self):
-        super(VideoPlayer, self).__init__()
-        self.initUI()
+# 检查前10个设备文件，可以根据需要更改范围
+max_devices = 10
 
-    def initUI(self):
-        self.setWindowTitle('Video Player')
-        self.setGeometry(300, 300, 800, 600)
-        self.media_player = QMediaPlayer(self)
-        self.video_widget = QVideoWidget(self)
-        self.setCentralWidget(self.video_widget)
-        self.media_player.setVideoOutput(self.video_widget)
-        self.open_button = QPushButton('Open Video', self)
-        self.open_button.clicked.connect(self.open_video)
-        self.statusBar().addPermanentWidget(self.open_button)
+for device_id in range(max_devices):
+    device_path = f'/dev/video{device_id}'
+    cap = cv2.VideoCapture(device_path)
 
-    def open_video(self):
-        video_path, _ = QFileDialog.getOpenFileName(self, 'Open Video', '', 'Video Files (*.avi *.mp4 *.mkv);;All Files (*)')
-        if video_path:
-            self.media_player.setMedia(QMediaContent(video_path))
-            self.media_player.play()
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    video_player = VideoPlayer()
-    video_player.show()
-    sys.exit(app.exec_())
+    if cap.isOpened():
+        print(f"Device {device_path} is available.")
+        cap.release()
+    else:
+        print(f"Device {device_path} is not available.")
